bab7ba13a7c4bec25d8612674aa0cab212c2ced0
Amrita Jana <amritajana777@gmail.com>
2026-08-15 19:25:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ clicklogo.addEventListener("click", function() {
     window.location.hash = "home";
 
 });
+
+const menuButton = document.querySelector("#menu-btn");
+
+const navLinks = document.querySelector(".nav-links");
+
+let menuOpen = false;
+
+menuButton.addEventListener("click", function() {
+
+    menuOpen = !menuOpen;
+
+});
